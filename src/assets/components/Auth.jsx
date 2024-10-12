1ba3b3415b27a009f5e6a25062fc757aa428437b
Abhishek Parihar <158466628+Abhi0556@users.noreply.github.com>
2024-10-12 10:33:06 +0530

    Update Auth.jsx
    images removed due to pixel issues.

--- src/assets/components/Auth.jsx
+++ src/assets/components/Auth.jsx
@@ -38,19 +38,8 @@ const Auth = ({ setUserAuthenticated }) => {
     <div className="flex min-h-screen">
       <div className="flex flex-col justify-between w-1/2 bg-gray-100 p-8">
         <div className="flex items-start">
-        <img
-            src="src/logo.jpg" 
-            alt="Quizo Logo"
-            className="w-30 h-20 items-center"
-          />
           <h1 className="text-4xl font-bold ml-4 text-black">Quizo</h1>
         </div>
-        <img
-            src="src/leftimage.svg" 
-            alt="Quizo Logo"
-            className="w-30 h-20 items-center"
-          />
-
       </div>
 
     
